06d6ba7abeef20f3ba651ef3985db7dcdccc5919
ambitionphp <huedate@gmail.com>
2017-08-16 10:44:37 -0400

    Main commit

---
+++ SarahahScraper/sarahah-username-scraper.php
@@ -0,0 +1,35 @@
+<?php
+$url = 'https://twitter.com/search?f=tweets&vertical=default&q=sarahah.com&src=typd';
+$refresh=10;
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+$response = curl_exec($curl);
+curl_close($curl);
+preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $response, $match);
+$check = [];
+$existing = file('collected.txt', FILE_IGNORE_NEW_LINES);
+foreach( $existing AS $e )
+{
+    $pieces = explode('|', $e);
+    $check[] = $pieces[0];
+}
+$usernames = [];
+$file = fopen('collected.txt', 'a');
+foreach( array_unique( $match[0] ) AS $url )
+{
+    if( strpos( $url, '.sarahah.com' ) !== false )
+    {
+        $pieces = explode('.', str_replace(['http://','https://'],'',$url));
+        if( $pieces[0] != 'www' && !in_array($pieces[0],$check) ) $usernames[] = $pieces[0];
+    }
+}
+foreach( array_unique( $usernames ) AS $username )
+{
+    fwrite($file, $username."|0\n");
+}
+fclose($file);
+print 'done';
+print '<meta http-equiv="refresh" content="'.$refresh.'">';
